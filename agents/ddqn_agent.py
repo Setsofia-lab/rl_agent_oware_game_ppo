a79d1cd70cee3e5111215346ea97297e7a92fbda
Setsofia-lab <setsofiaeli@gmail.com>
2024-12-26 18:13:56 -0500

    training and evaluation of the oware baords

--- agents/ddqn_agent.py
+++ agents/ddqn_agent.py
@@ -1,13 +1,61 @@
+import random
+import numpy as np
+from collections import deque
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
 class DDQNAgent:
     def __init__(self, state_size, action_size):
-        # Initialize Q-network and replay buffer
-        pass
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.99  # Discount factor
+        self.epsilon = 1.0  # Exploration rate
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+        self.target_model = self._build_model()
+        self.update_target_model()
+
+    def _build_model(self):
+        model = Sequential()
+        model.add(Dense(128, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
-        # Use epsilon-greedy strategy for action selection
-        pass
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        q_values = self.model.predict(np.array([state]), verbose=0)
+        return np.argmax(q_values[0])
 
-    def train(self, experience_batch):
-        # Update Q-network
-        pass
+    def train(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = self.model.predict(np.array([state]), verbose=0)[0]
+            if done:
+                target[action] = reward
+            else:
+                next_action = np.argmax(self.model.predict(np.array([next_state]), verbose=0)[0])
+                target[action] = reward + self.gamma * self.target_model.predict(np.array([next_state]), verbose=0)[0][next_action]
+
+            self.model.fit(np.array([state]), np.array([target]), epochs=1, verbose=0)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
 
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,19 @@
+from games.game_controller import GameController
+from agents.random_agent import RandomAgent
+
 def calculate_win_rate(agent, opponent, num_games):
-    # Simulate games and return win rate
-    pass
+    wins = 0
+    for _ in range(num_games):
+        game_controller = GameController(agent, opponent)
+        winner = game_controller.play_game()
+        if winner == agent:
+            wins += 1
+    return wins / num_games
 
 def calculate_average_reward(agent, num_games):
-    # Simulate games and compute average reward
-    pass
+    total_reward = 0
+    for _ in range(num_games):
+        game_controller = GameController(agent, RandomAgent())
+        total_reward += game_controller.play_game()
+    return total_reward / num_games
+
